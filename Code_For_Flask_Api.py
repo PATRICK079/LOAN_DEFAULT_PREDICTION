c446d171e9663002cc4dbdef133688cfef99ae56
Patrick Edosoma <157173680+PATRICK079@users.noreply.github.com>
2024-07-24 15:53:49 +0300

    Update Code_For_Flask_Api.py

--- Code_For_Flask_Api.py
+++ Code_For_Flask_Api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import numpy as np
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -10,20 +11,25 @@ def predict():
     try:
         feat_data = request.json
         df = pd.DataFrame(feat_data)
-        df = pd.get_dummies(df, drop_first= True)
+        df = pd.get_dummies(df, drop_first=True)
         df = df.reindex(columns=col_names, fill_value=0)  
-         
+        
         df = scaler.transform(df)
-        prediction = list(model.predict(df))
+        predictions = model.predict(df)
+        prediction_messages = ['(1) -> This client will not be able to pay back when given a loan' if pred == 1 
+        else '(0) -> This client would pay back when given a loan' for pred in predictions]
 
-        return jsonify({'prediction': str(prediction)})
+        return jsonify({'prediction': prediction_messages})
     except Exception as e:
-        return jsonify({'error': str(e)}),400
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    model = joblib.load("/Users/sot/final_model.pk1")  
-    col_names = joblib.load('/Users/sot/col_names.pk1') 
-    scaler =   joblib.load('final_scaler.pk1')                 
+    model_path = os.getenv('MODEL_PATH')
+    col_names_path = os.getenv('COL_NAMES_PATH')
+    scaler_path = os.getenv('SCALER_PATH')
 
+    model = joblib.load(model_path)  
+    col_names = joblib.load(col_names_path) 
+    scaler = joblib.load(scaler_path)                 
 
     app.run(debug=True)
